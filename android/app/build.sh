f84180d989d0c2b1c90b17d1459bd67fe0ae47ec
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-12-02 16:06:14 +0700

    success build APK

---
+++ android/app/build.sh
@@ -0,0 +1,26 @@
+
+
+
+ffmpeg -i "/Users/aamuzakii/Movies/au89.mov" -vn -acodec libmp3lame -q:a 2 "/Users/aamuzakii/Movies/au89.mp3"
+
+
+cd /Users/aamuzakii/zaki/eject/my-app
+
+
+pkill -f gradle
+rm -rf android/.gradle/8.14.3/executionHistory/*.lock
+# Kill Gradle/Java daemons
+pkill -f gradle
+pkill -f java
+
+# Remove project build artifacts
+rm -rf android/app/build
+rm -rf android/.gradle
+rm -rf android/app/.cxx
+rm -rf node_modules/.cache
+
+# Optional: remove global Gradle caches if low on space
+rm -rf ~/.gradle/caches
+
+# Reinstall dependencies
+npm install  # or yarn install
