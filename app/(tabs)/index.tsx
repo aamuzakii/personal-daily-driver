161a32f1ed40d8faf6b52b326e0164d3d8a36bab
Abdullah Al Muzaki <aamuzakii@gmail.com>
2026-01-13 08:37:13 +0700

    intent

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -79,7 +79,7 @@ export default function HomeScreen() {
     { id: 'zikir-pagi', title: 'Zikir Pagi', link: 'https://www.instagram.com/boris.tan/', score: 10, done: false },
     { id: 'zikir-petang', title: 'Zikir Petang', link: 'fiqih_yt_random', score: 10, done: false },
     { id: 'duha', title: 'Shalat Dhuha', link: 'https://www.youtube.com/watch?v=YDvsBbKfLPA', score: 10, done: false },
-    { id: 'witir', title: 'Shalat Witir', link: 'https://play.google.com/store/apps/details?id=com.ichi2.anki&hl=en', score: 10, done: false },
+    { id: 'witir', title: 'Shalat Witir', link: 'https://play.google.com/store/apps/details?id=us.nobarriers.elsa&hl=en', score: 10, done: false },
     { id: 'mutun', title: 'Familiar Mutun + Riyadhushhalihin: 10 menit sehari', link: 'https://www.youtube.com/watch?v=jyeikSj0Qt4&list=PLR-wSuUv3U1z1If_ANqpdtcjyV70wmqVK&index=55', score: 10, done: false },
     { id: 'recite', title: 'Baca Quran 1 Juz / 30 menit', link: 'https://play.google.com/store/apps/details?id=com.quran.labs.androidquran&hl=en', score: 10, done: false },
     { id: 'hsk', title: 'HSK 5', link: 'https://www.youtube.com/watch?v=vTVuuJ5xBco', score: 3, done: false },
